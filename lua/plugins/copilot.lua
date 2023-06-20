14c999301b0cab84142a5bd31b1be4656223a097
Derrik Milligan <derrik@applicantpro.com>
2023-06-20 13:29:37 -0600

    Fixed up co-pilot. Got phpactor working with the lspconfig.

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,6 +1,16 @@
 return {
-  "github/copilot.vim",
-  opt = {
-
-  }
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  build = ":Copilot auth",
+  opts = {
+    suggestion = {
+      enabled = false,
+      auto_trigger = true,
+    },
+    panel = { enabled = false },
+    filetypes = {
+      markdown = true,
+      help = true,
+    },
+  },
 }
--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,6 +1,23 @@
 return {
   "neovim/nvim-lspconfig",
+
+  ---@class PluginLspOpts
   opts = {
     autoformat = false,
-  },
+
+    ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
+    setup = {
+      phpactor = function(_, opts)
+        opts.cmd = { "php82", "/usr/bin/phpactor", "language-server" }
+
+        opts.init_options = {
+          ['language_server_phpstan.enabled'] = true,
+          ['language_server_psalm.enabled'] = false,
+        }
+
+        vim.g.phpactorPhpBin = "/usr/bin/php82"
+      end
+    }
+  }
+
 }
