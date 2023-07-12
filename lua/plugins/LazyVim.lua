2acffa2f5f2d36c4c98ea89ac0dd38758497352e
Derrik Milligan <derrik@applicantpro.com>
2023-07-12 15:16:23 -0600

    Few theme tweaks.

--- lua/plugins/LazyVim.lua
+++ lua/plugins/LazyVim.lua
@@ -4,6 +4,51 @@ return {
   -- { "ellisonleao/gruvbox.nvim" },
 
   {
+    'uloco/bluloco.nvim',
+    -- lazy = false,
+    -- priority = 1000,
+    dependencies = { 'rktjmp/lush.nvim' },
+    config = {
+      transparent = true,
+    },
+  },
+
+  {
+    'oxfist/night-owl.nvim',
+  },
+
+  {
+    'sainnhe/edge',
+  },
+
+  {
+    'sainnhe/gruvbox-material',
+    lazy = false,
+    priority = 1000,
+    init = function ()
+      vim.g["background"] = 'dark'
+      vim.g["gruvbox_material_transparent_background"] = true
+      vim.g["gruvbox_material_background"] = 'hard'
+      vim.g["gruvbox_material_italic_comment"] = true
+      vim.g["gruvbox_material_italic"] = true
+      vim.g["gruvbox_material_bold"] = true
+      vim.g["gruvbox_material_foreground"] = 'original'
+    end
+  },
+
+  {
+    'tiagovla/tokyodark.nvim',
+    -- lazy = false,
+    -- priority = 1000,
+    init = function ()
+      vim.g["tokyodark_transparent_background"] = true
+      vim.g["tokyodark_enable_italic_comment"] = true
+      vim.g["tokyodark_enable_italic"] = true
+      vim.g["tokyodark_color_gamma"] = '1.0'
+    end
+  },
+
+  {
     "ray-x/aurora",
     init = function()
       vim.g["aurora_darker"] = true
@@ -17,7 +62,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "aurora",
+      colorscheme = "gruvbox-material",
     },
   },
 
